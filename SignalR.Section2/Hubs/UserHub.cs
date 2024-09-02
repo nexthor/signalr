1199e1d8886f4d1a6c21ad77309d3bed7202a2ea
Nestor Danilo Mendoza <nestordanilo.mendoza@concentrix.com>
2024-09-02 14:18:33 -0600

    first hub is working, sending and receiving messages

--- SignalR.Section2/Hubs/UserHub.cs
+++ SignalR.Section2/Hubs/UserHub.cs
@@ -4,5 +4,12 @@ namespace SignalR.Section2.Hubs
 {
     public class UserHub : Hub
     {
+        public static int TotalViews { get; set; } = 0;
+
+        public async Task GetTotalViews()
+        {
+            TotalViews++;
+            await Clients.All.SendAsync("updateTotalViews", TotalViews);
+        }
     }
 }
