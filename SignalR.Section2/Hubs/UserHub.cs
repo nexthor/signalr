fff144f83c7271f80a4ce7ce528251d41981bc39
Nestor Danilo Mendoza <nestordanilo.mendoza@concentrix.com>
2024-09-03 16:11:51 -0600

    demo section 2 is done

--- SignalR.Section2/Hubs/UserHub.cs
+++ SignalR.Section2/Hubs/UserHub.cs
@@ -5,11 +5,26 @@ namespace SignalR.Section2.Hubs
     public class UserHub : Hub
     {
         public static int TotalViews { get; set; } = 0;
+        public static int TotalUsers { get; set; } = 0;
+
+        public override async Task OnConnectedAsync()
+        {
+            TotalUsers++;
+            await Clients.All.SendAsync("updateTotalUsers", TotalUsers);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            TotalUsers--;
+            await Clients.All.SendAsync("updateTotalUsers", TotalUsers);
+        }
 
         public async Task GetTotalViews()
         {
             TotalViews++;
             await Clients.All.SendAsync("updateTotalViews", TotalViews);
         }
+
+
     }
 }
