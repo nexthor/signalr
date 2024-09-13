f0d8f3a143e8aff98fc9f03489e314322ac8cbe2
Nestor Danilo Mendoza <nestordanilo.mendoza@concentrix.com>
2024-09-13 10:52:58 -0600

    adding a notification form to the project

---
+++ SignalR.Sections/Controllers/Section5Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalR.Sections.Controllers
+{
+    public class Section5Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
